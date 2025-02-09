9936ac9fc6f5a583cc560d8ccb9e4c50dd1ba557
Bryce MacInnis <brycemacinnis@T96DC.WPA.Dal.Ca>
2025-02-09 18:02:08 -0400

    Adds basic version of GP

---
+++ gp/variation.py
@@ -0,0 +1,31 @@
+from copy import deepcopy
+
+from gp.program import Program
+import random
+
+def get_crossover_point(program):
+    candidates = []
+
+    def traverse(node, parent, attr, is_root):
+        if not is_root:
+            candidates.append((node, parent, attr))
+        if not node.is_leaf_():
+            traverse(node.left, parent=node, attr='left', is_root=False)
+            traverse(node.right, parent=node, attr='right', is_root=False)
+
+    traverse(program, parent=None, attr=None, is_root=True)
+    return random.choice(candidates) if candidates else (program, None, None)
+
+def crossover(A, B):
+    child1 = deepcopy(A)
+    child2 = deepcopy(B)
+    node1, parent1_ref, attr1 = get_crossover_point(child1)
+    node2, parent2_ref, attr2 = get_crossover_point(child2)
+
+    if parent1_ref is None or parent2_ref is None:
+        return child2, child1
+
+    setattr(parent1_ref, attr1, node2)
+    setattr(parent2_ref, attr2, node1)
+
+    return child1, child2
